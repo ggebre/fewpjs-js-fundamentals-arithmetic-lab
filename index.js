e74f1506cf296ea4b2ecb2ae5b7cf91dfceda0bc
Getu Gebre <getu.gebre.gg@gmail.com>
2021-01-17 12:10:31 -0600

    commit tree 5047107e27f4319e20136f3c0430be1a3760d540

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * Math.floor(100)) + 1
+
+const mod = 10 % 6
+
+const max = Math.max(...[1,2,20,5]) 
+
